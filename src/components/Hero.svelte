ecbaadc56e329d0d58dadbc67672e833abed5bf2
Patrick Kavanagh <kreollc@hotmail.com>
2022-08-31 13:13:15 -0400

    first commit

---
+++ src/components/Hero.svelte
@@ -0,0 +1,9 @@
+<div class="hero">
+  <div class="banner">
+    <h1>foam, shave, grow</h1>
+    <p>embrace your choices - we do!</p>
+    <slot>
+      <!-- optional fallback-->
+    </slot>
+  </div>
+</div>
---
+++ src/strapi/URL.js
@@ -0,0 +1,5 @@
+// heroku hosted strapi database
+export default 'https://razor-store.herokuapp.com'
+
+// below was local
+// export default 'http://localhost:1337'
